97e9cce6ddb5ce90b9f4d1f4e0a9653ddef69f5b
Aurelien COLOMBET <colombet.aurelien@etu.unice.fr>
2014-06-11 19:19:02 +0200

    Ajout du premier test en asm

---
+++ Simul_Proc-linux-m64/tests/halt.asm
@@ -0,0 +1,16 @@
+ TEXT 30
+
+        // Programme principal
+main EQU *
+        NOP
+
+        END
+        
+//-----------------
+// Données et pile
+//-----------------
+        DATA 30
+        
+        WORD 1234
+        
+        END
